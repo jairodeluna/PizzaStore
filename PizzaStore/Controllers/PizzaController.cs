5fee83721afe30a7e2f509271eae594da59bac3b
livin <livin@DESKTOP-43ASOFV>
2024-04-21 00:49:40 +0800

    Added Pizza Controller, Views and removed unecessary default views

---
+++ PizzaStore/Controllers/PizzaController.cs
@@ -0,0 +1,66 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using PizzaStore.Models;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace PizzaStore.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly PizzaHubContext _context;
+
+        public PizzaController(PizzaHubContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _context.Pizzas.ToList();
+            return View(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImportPizza()
+        {
+            try
+            {
+                var file = Request.Form.Files.FirstOrDefault();
+
+                if (file != null && file.Length > 0)
+                {
+                    using (var reader = new StreamReader(file.OpenReadStream()))
+                    {
+                        using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+                        {
+                            var records = csvReader.GetRecords<dynamic>().ToList();
+
+                            foreach (var record in records)
+                            {
+                                var pizzaProduct = new Pizza
+                                {
+                                    PizzaId = record.pizza_id,
+                                    PizzaType = record.pizza_type_id,
+                                    Size = record.size,
+                                    Price = Convert.ToDouble(record.price),
+                                };
+
+                                _context.Pizzas.Add(pizzaProduct);
+                            }
+
+                            await _context.SaveChangesAsync();
+                        }
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- PizzaStore/Startup.cs
+++ PizzaStore/Startup.cs
@@ -27,7 +27,7 @@ namespace PizzaStore
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
             if (!env.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Pizza/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -42,7 +42,7 @@ namespace PizzaStore
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Pizza}/{action=Index}/{id?}");
             });
         }
     }
